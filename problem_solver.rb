b942eeb9c477b0124c6a1d719f75a2385a9761c8
GonzaJuan <gonzajuan@pacbell.net>
2018-01-15 22:19:22 -0800

    commit at 10:15pm

--- problem_solver.rb
+++ problem_solver.rb
@@ -2,30 +2,30 @@ class ProblemSolver
   def solve_easy_problem
     # this is a new change after forking the project.
 
-    # UPDATE FROM GONZAJUAN AT 7:09PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
   end
   def solve_harder_problem
     # this is a new change after forking the project.
     # this is a new change after forking the project.
 
-    # UPDATE FROM GONZAJUAN AT 7:09PM
-    # UPDATE FROM GONZAJUAN AT 7:09PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
   end
   def solve_tough_problem
     # this is a new change after forking the project.
     # this is a new change after forking the project.
     # this is a new change after forking the project.
 
-    # UPDATE FROM GONZAJUAN AT 7:09PM
-    # UPDATE FROM GONZAJUAN AT 7:09PM
-    # UPDATE FROM GONZAJUAN AT 7:09PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
+    # UPDATE FROM GONZAJUAN AT 10:18PM
   end
   def myMethod
     darwin = Darwin.new(problem)
     darwin.survive
 
     if survive
-      # UPDATE FROM GONZAJUAN AT 7:09PM
+      # UPDATE FROM GONZAJUAN AT 10:18PM
       get_goldmedal()
     end
   end
@@ -36,8 +36,8 @@ class ProblemSolver
 
     if answered
       # Add achievement point to global counter.
-      # UPDATE FROM GONZAJUAN AT 7:09PM
-      # UPDATE FROM GONZAJUAN AT 7:09PM
+      # UPDATE FROM GONZAJUAN AT 10:18PM
+      # UPDATE FROM GONZAJUAN AT 10:18PM
     end
   end
 end
