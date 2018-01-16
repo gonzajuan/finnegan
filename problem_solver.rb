9c849478addf58fb93f3ae3a3750486801462804
Juan Gonzalez <gonzajuan@pacbell.net>
2018-01-15 17:08:00 -0800

    new method

--- problem_solver.rb
+++ problem_solver.rb
@@ -6,5 +6,12 @@ class ProblemSolver
   def solve_tough_problem
   end
   def myMethod
+    darwin = Darwin.new(problem)
+    darwin.survive
+  end
+  def prolog_solver(description, question)
+    prolog = prolog.new
+    prolog.describe_world(description)
+    prolog.ask(question)
   end
 end
