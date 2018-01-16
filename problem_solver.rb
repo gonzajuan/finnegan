31b08a423cb19ae8e96e336d1743842f820f47d3
Juan Gonzalez <gonzajuan@pacbell.net>
2018-01-15 18:48:38 -0800

    new changes after fork

--- problem_solver.rb
+++ problem_solver.rb
@@ -1,17 +1,31 @@
 class ProblemSolver
   def solve_easy_problem
+    # this is a new change after forking the project.
   end
   def solve_harder_problem
+    # this is a new change after forking the project.
+    # this is a new change after forking the project.
   end
   def solve_tough_problem
+    # this is a new change after forking the project.
+    # this is a new change after forking the project.
+    # this is a new change after forking the project.
   end
   def myMethod
     darwin = Darwin.new(problem)
     darwin.survive
+
+    if survive
+      get_goldmedal()
+    end
   end
   def prolog_solver(description, question)
     prolog = prolog.new
     prolog.describe_world(description)
     prolog.ask(question)
+
+    if answered
+      # Add achievement point to global counter.
+    end
   end
 end
